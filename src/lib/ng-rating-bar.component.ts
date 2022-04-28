8ecc19fa93839993f676e9dacac95e68f72809d3
Zulkarnain Shah <m.zulkarnain.shah@gmail.com>
2022-04-28 05:46:59 +0530

    Total stars, custom star image, max stars features added

--- src/lib/ng-rating-bar.component.ts
+++ src/lib/ng-rating-bar.component.ts
@@ -7,19 +7,27 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class NgRatingBarComponent implements OnInit {
   @Input() rating: number | undefined;
-  emptyStars: number = 5;
+  @Input() filledImgSrc?: string = "../../assets/star_filled.png";
+  @Input() emptyImgSrc?: string = "../../assets/star_empty.png";
+  @Input() halfFilledImgSrc?: string = "../../assets/star_half.png";
+  @Input() size: string = "20px";
+  @Input() totalStars: number = 5;
+
   wholeNumbers: number | undefined;
   hasFractions: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {
+    if (this.rating != null && this.rating > this.totalStars) {
+      this.rating = this.totalStars;
+    }
     let tokens = this.rating ? this.rating?.toString().split(".") : null;
     if (tokens) {
       var wholeNumbers = tokens[0] ? tokens[0] : this.rating?.toString();
       this.hasFractions = tokens[1] ? true : false;
       this.wholeNumbers = wholeNumbers ? parseInt(wholeNumbers) : undefined;
-      this.emptyStars = this.emptyStars - Math.ceil(this.rating!);
+      this.totalStars = this.totalStars - Math.ceil(this.rating!);
     }
   }
 }
