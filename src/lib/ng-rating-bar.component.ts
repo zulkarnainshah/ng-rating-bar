15541d3f75127aebc339394ec2c348a1dd7e6d8c
Zulkarnain Shah <m.zulkarnain.shah@gmail.com>
2022-04-30 13:22:20 +0530

    Change stars on hover and change rating on click and send event to container

--- src/lib/ng-rating-bar.component.ts
+++ src/lib/ng-rating-bar.component.ts
@@ -1,36 +1,78 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: "ng-rating-bar",
-  templateUrl: "./ng-rating-bar.component.html",
-  styleUrls: ["./ng-rating-bar.component.scss"],
+  selector: 'ng-rating-bar',
+  templateUrl: './ng-rating-bar.component.html',
+  styleUrls: ['./ng-rating-bar.component.scss'],
 })
 export class NgRatingBarComponent implements OnInit {
   @Input() rating: number = 0;
-  @Input() filledImgSrc?: string = "../../assets/ng-rating-bar/star_filled.png";
-  @Input() emptyImgSrc?: string = "../../assets/ng-rating-bar/star_empty.png";
+  @Input() filledImgSrc?: string = '../../assets/ng-rating-bar/star_filled.png';
+  @Input() emptyImgSrc?: string = '../../assets/ng-rating-bar/star_empty.png';
   @Input() halfFilledImgSrc?: string =
-    "../../assets/ng-rating-bar/star_half.png";
-  @Input() size: string = "15px";
+    '../../assets/ng-rating-bar/star_half.png';
+  @Input() size: string = '15px';
   @Input() totalStars: number = 5;
+  @Output() ratingChanged = new EventEmitter<number>();
 
-  wholeNumbers: number = 0;
+  editableRating: number = 0;
+  filledStars: number = 0;
   hasFractions: boolean = false;
+  emptyStars: number = 5;
 
   constructor() {}
 
   ngOnInit(): void {
+    this.editableRating = this.rating;
+    this.calculateStars();
+  }
+
+  private calculateStars() {
     //If totalStars supplied by user is a number with decimal point
     this.totalStars = Math.ceil(this.totalStars);
-    if (this.rating != null && this.rating > this.totalStars) {
-      this.rating = this.totalStars;
+    if (this.editableRating != null && this.editableRating > this.totalStars) {
+      this.editableRating = this.totalStars;
     }
-    let tokens = this.rating ? this.rating?.toString().split(".") : null;
+    let tokens = this.editableRating
+      ? this.editableRating?.toString().split('.')
+      : null;
     if (tokens) {
-      var wholeNumbers = tokens[0] ? tokens[0] : this.rating?.toString();
+      var wholeNumbers = tokens[0]
+        ? tokens[0]
+        : this.editableRating?.toString();
       this.hasFractions = tokens[1] ? true : false;
-      this.wholeNumbers = wholeNumbers ? parseInt(wholeNumbers) : 0;
-      this.totalStars = this.totalStars - Math.ceil(this.rating!);
+      this.filledStars = wholeNumbers ? parseInt(wholeNumbers) : 0;
+      this.emptyStars = this.totalStars - Math.ceil(this.editableRating!);
     }
+  }
+
+  mouseEnteredFilledStar(index: number) {
+    this.filledStars = index + 1;
+    this.editableRating = this.filledStars;
+    this.calculateStars();
+  }
+
+  mouseEnteredEmptyStar(index: number) {
+    this.editableRating = this.filledStars + (index + 1);
+    this.calculateStars();
+  }
+
+  mouseLeftFilledStar(index: number) {
+    this.editableRating = this.filledStars - (index - 1);
+    this.calculateStars();
+  }
+
+  mouseLeftEmptyStar(index: number) {}
+
+  onMouseLeftContainer() {
+    this.editableRating = this.rating;
+    this.calculateStars();
+  }
+
+  filledStarClicked(index: number) {
+    this.rating = index + 1;
+    this.editableRating = this.rating;
+    this.calculateStars();
+    this.ratingChanged?.emit(this.rating);
   }
 }
